3d899989bc1f8e6a1cb1d8965c8b8e8e4bbe8fe9
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-18 20:12:14 -0300

    nome da pasta

--- api_cadastrologin/src/controllers/authController.js
+++ api_cadastrologin/src/controllers/authController.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcrypt js');
 const User = require('../models/User');
 const router = express.Router();
 
+/*rota de registro */
 router.post('/register', async (req, res) => {
   const { email } = req.body;
 
@@ -17,5 +19,23 @@ router.post('/register', async (req, res) => {
     return res.status(400).send({ error: 'Falha no registro' });
   }
 });
+
+/*rota de autenticacao*/
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user)
+    return res.status(400).send({ error: 'Usuário não encontrado' });
+
+  if (!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Senha inválida' })
+
+  res.send({ user });
+});
+
+
+
 
 module.exports = app => app.use('/auth', router);
