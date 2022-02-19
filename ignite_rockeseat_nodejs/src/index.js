1f4c294a9ea009aaa16bad5bb1abecae934edea1
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-19 11:10:23 -0300

    tipos de parametros

--- ignite_rockeseat_nodejs/src/index.js
+++ ignite_rockeseat_nodejs/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 /*metodos httpp 
 GET - buscar info dentro do servidor
@@ -9,15 +10,32 @@ PATH - alterar info especifica no Servidor
 DELETE - deletar info no servidor
 */
 
+/*tipo de parametros
+Route params - "depois da barra - exemplo("/courses/:id") - indentificar/editar/deletar/buscar um recurso
+Query params - quando quiser realizar paginação/filtros
+Body params - objetos passados para inserção/alteração do recurso (JSON)
+*/
 app.get('/courses', (req, res) => {
+
+  const query = req.query;
+  console.log(query)
+
   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post('/courses', (req, res) => {
+
+  const body = req.body
+  console.log(body);
+
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put('/courses/:id', (req, res) => {
+
+  const { id } = req.params;
+  console.log(id);
+
   return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
