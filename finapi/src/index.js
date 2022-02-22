176d24d41d2f5cc50d8a8a7bc3e9e6f5be9551d6
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-21 23:20:53 -0300

    listando dados

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -110,6 +110,12 @@ app.put('/account', verifyIfExistsAccountCPF, (req, res) => {
   return res.status(201).send({ message: 'Account successfully changed!' })
 });
 
+app.get('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer);
+});
+
 
 app.listen(3000);
 console.log('server running . . .');
