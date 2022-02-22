d8df2de9cad3c3ff69aaacd2117b5a41e60725f1
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-02-21 23:35:23 -0300

    mostrando o saldo e finalizando a api finapi

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -124,6 +124,13 @@ app.delete('/account', verifyIfExistsAccountCPF, (req, res) => {
   return res.status(200).json({ customers });
 });
 
+app.get('/balance', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  const balance = getBalance(customer.statement);
+
+  return res.json(balance);
+});
+
 
 app.listen(3000);
 console.log('server running . . .');
